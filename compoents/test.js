3cc151fb983b124367b6ed63147867b0ded7ed50
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-03-01 16:44:36 -0700

    working imports

---
+++ compoents/test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { StyleSheet, Text, View } from 'react-native';
+
+function Example(props)
+{
+  return(
+    <Text>sample text</Text>
+  )
+}
+
+export default Example
