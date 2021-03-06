6933be8b5b3a059fd7420f477150737284cfc310
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-03-06 11:07:55 -0700

    Week 1 Assignment Task 3

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,5 +1,19 @@
 import React, {Component} from "react"
-import {ScrollView} from "react-native"
+import {ScrollView, Text, FlatList} from "react-native"
+import {Card, ListItem} from "react-native-elements"
+import {PARTNERS} from "../shared/partners"
+
+
+
+function Mission(props)
+{
+    return(
+
+        <Card title="Our Mission" wrapperStyle={{margin: 10}}>
+               <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+        </Card>
+    )
+}
 
 class About extends Component
 
@@ -7,16 +21,41 @@ class About extends Component
     constructor(props)
     {
         super(props)
+        this.state={
+            partners: PARTNERS
+        }
     }
 
     static navigationOptions = {
         title: 'About Us'
     }
 
+    renderPartner({item})
+    {
+    
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                />
+            );
+     };
+
+
     render(){
 
         return(
-           <ScrollView></ScrollView>
+           <ScrollView>
+               <Mission />
+               <Card title="Community Partners">
+                   <FlatList 
+                   data={this.state.partners} 
+                   renderItem={this.renderPartner}
+                   keyExtractor={item => item.id.toString()} 
+                   />
+               </Card>
+           </ScrollView>
         )
     }
 
